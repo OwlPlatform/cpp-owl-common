bbff581bc5d042e3500a64fd091687359b78aa0f
Ben Firner <bfirner@eden.rutgers.edu>
2012-06-25 11:55:21 -0400

    Adding grail_types file.

---
+++ include/grail_types.hpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * This file defines types that exist in the world model.
+ * Endian differences stop packed structs from being read in directly from a
+ * raw byte buffer to the struct.
+ ******************************************************************************/
+
+#ifndef __GRAIL_TYPES_HPP__
+#define __GRAIL_TYPES_HPP__
+
+#include <sample_data.hpp>
+#include <netbuffer.hpp>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+namespace grail_types {
+
+  //A transmitter entry holds a physical layer and id.
+  struct transmitter {
+    uint8_t phy;
+    uint128_t id;
+  } __attribute__((packed));
+
+  bool operator<(const transmitter& a, const transmitter& b);
+  bool operator==(const transmitter& a, const transmitter& b);
+
+  std::ostream& operator<<(std::ostream& out, const transmitter& t);
+
+  transmitter readTransmitterFromBuffer(BuffReader& reader);
+  transmitter readTransmitter(std::vector<unsigned char>& buff);
+  //Write the transmitter to a buffer and return the bytes written
+  uint32_t writeTransmitter(transmitter t, std::vector<unsigned char>& buff);
+
+  template<typename T>
+    std::vector<T> unpackGRAILVector(std::function<T (BuffReader&)> read_fun, std::vector<unsigned char>& buff) {
+      BuffReader reader(buff);
+      std::vector<T> results;
+      uint32_t total = reader.readPrimitive<uint32_t>();
+      for (;0 < total; --total) {
+        results.push_back(read_fun(reader));
+      }
+      return results;
+    }
+
+};
+
+#endif
+
---
+++ src/grail_types.cpp
@@ -0,0 +1,36 @@
+#include <grail_types.hpp>
+
+bool grail_types::operator<(const transmitter& a, const transmitter& b) {
+  return a.phy < b.phy or
+    ( (a.phy == b.phy) and a.id < b.id);
+}
+
+bool grail_types::operator==(const transmitter& a, const transmitter& b) {
+  return a.phy == b.phy and a.id == b.id;
+}
+
+std::ostream& grail_types::operator<<(std::ostream& out, const transmitter& t) {
+  return out << (unsigned int)t.phy << '.' << t.id;
+}
+
+grail_types::transmitter grail_types::readTransmitterFromBuffer(BuffReader& reader) {
+  transmitter t;
+  reader.convertPrimitive(t.phy);
+  t.id.upper = reader.readPrimitive<uint64_t>();
+  t.id.lower = reader.readPrimitive<uint64_t>();
+  return t;
+}
+
+grail_types::transmitter grail_types::readTransmitter(std::vector<unsigned char>& buff) {
+  transmitter t;
+  t.phy = readPrimitive<unsigned char>(buff, 0);
+  t.id.upper = readPrimitive<uint64_t>(buff, 1);
+  t.id.lower = readPrimitive<uint64_t>(buff, 1+sizeof(uint64_t));
+  return t;
+}
+
+uint32_t grail_types::writeTransmitter(transmitter t, std::vector<unsigned char>& buff) {
+  return pushBackVal<uint8_t>(t.phy, buff) +
+    pushBackVal<uint64_t>(t.id.upper, buff) + pushBackVal<uint64_t>(t.id.lower, buff);
+}
+
