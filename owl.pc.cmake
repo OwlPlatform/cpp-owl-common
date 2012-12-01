86ec97877eff5f5316ecba5608aec4fac359f266
Ben Firner <bfirner@eden.rutgers.edu>
2012-12-01 13:52:51 -0500

    Changing lib name to libowl-common.

--- owl.pc.cmake
+++ owl.pc.cmake
@@ -1,7 +1,7 @@
 Name: owl
 Description: Owl platform networking library
 Requires:
-Version: @LibOwl_VERSION_MAJOR@
-Libs: -L@LIB_DESTINATION@ -lowl
+Version: @LibOwl_Common_VERSION_MAJOR@
+Libs: -L@LIB_DESTINATION@ -lowl-common
 Cflags: -I@INCLUDE_INSTALL_DIR@
 
